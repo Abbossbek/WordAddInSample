7f8dda52229cfdff357d8d8db61f854e75c7be0e
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-04-23 15:57:27 +0500

    Добавьте файлы проекта.

---
+++ WordAddInSample/Ribbon.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace WordAddInSample
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TabHome = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.TabHome.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabHome
+            // 
+            this.TabHome.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.TabHome.ControlId.OfficeId = "TabHome";
+            this.TabHome.Groups.Add(this.group1);
+            this.TabHome.Label = "TabHome";
+            this.TabHome.Name = "TabHome";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Label = "Test group";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Image = global::WordAddInSample.Properties.Resources.Горы;
+            this.button1.Label = "test";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.TabHome);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.TabHome.ResumeLayout(false);
+            this.TabHome.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab TabHome;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
---
+++ WordAddInSample/ThisAddIn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace WordAddInSample
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+
+        }
+        //private void AddButtonsToMenu()
+        //{
+        //    RibbonButton tempButton = Globals.Factory.GetRibbonFactory().CreateRibbonButton();
+        //    tempButton.Label = "Button 1";
+        //    tempButton.ControlSize =
+        //        Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+        //    tempButton.Description = "My Ribbon Button";
+        //    tempButton.ShowImage = true;
+        //    tempButton.Image = Properties.Resources.Горы;
+        //    tempButton.KeyTip = "A1";
+        //    Globals.Ribbons.GetRibbon(Ribbon).Add(tempButton);
+
+        //}
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Код, автоматически созданный VSTO
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
